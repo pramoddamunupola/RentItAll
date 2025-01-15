8d302f6d266571bf2f96d382c4079f6e75db00bf
GIHAN PRAMOD <gihanpramod86@gmail.com>
2025-01-15 22:41:43 +0530

    Update SignUpdata.php

--- SignUpdata.php
+++ SignUpdata.php
@@ -2,17 +2,17 @@
 include("connection.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Check if all fields are filled
+  
     if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email']) || empty($_POST['phone'])) {
         echo "Please fill in all fields!";
     } else {
-        // Sanitize and escape input to prevent SQL injection
+        
         $username = mysqli_real_escape_string($conn, $_POST['username']);
         $password = mysqli_real_escape_string($conn, $_POST['password']);
         $email = mysqli_real_escape_string($conn, $_POST['email']);
         $phone = mysqli_real_escape_string($conn, $_POST['phone']);
         
-        // SQL query to insert data into the database
+       
         $sql = "INSERT INTO account_details (Username, Password, Email, PhoneNumber) 
                 VALUES ('$username', '$password', '$email', '$phone')";
 
