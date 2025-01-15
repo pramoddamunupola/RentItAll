097ae47aae84f70023a8c2656c8a275a27755e4f
GIHAN PRAMOD <gihanpramod86@gmail.com>
2025-01-15 18:17:16 +0530

    Update SignIn.php

--- SignIn.php
+++ SignIn.php
@@ -63,19 +63,17 @@
             margin-top: 20px;
         }
         .back-button{
-            padding: 10px 10px;
+            padding: 10px 20px;
             font-size: 16px;
             border: 1px solid white;
             border-radius: 5px;
             cursor: pointer;
             color: white;
             background-color: transparent;
-            transition: background-color 0.3s, border-color 0.3s;
         }
         .buttons button {
             padding: 10px 20px;
             font-size: 16px;
-            border: none;
             border-radius: 5px;
             cursor: pointer;
         }
@@ -83,11 +81,13 @@
         .buttons .sign-in {
             background-color: #4CAF50;
             color: white;
+            border: none;
         }
 
         .buttons .cancel {
             background-color: #f44336;
             color: white;
+            border: none;
         }
 
         .login-link {
@@ -108,7 +108,6 @@
 <body>
 
     <div class="container">
-    <button class="back-button" onclick="window.location.href='index.php'">< Back</button>
         <form method="POST" action="SignIndata.php">
             <table>
                 <tr>
@@ -124,7 +123,11 @@
                     <td><input type="password" id="password" name="password" placeholder="Password" required></td>
                 </tr>
                 <tr>
-                    <td></td>
+                    <td>
+                        <div class="buttons">
+                            <button class="back-button" onclick="window.location.href='index.php'">< Back</button>
+                        </div>
+                    </td>
                     <td>
                         <div class="buttons">
                             <button type="reset" class="cancel">Clear</button>
