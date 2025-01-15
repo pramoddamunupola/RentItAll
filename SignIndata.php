ddcbd15f3577b9356d0721b8293c1bd28c8f8d44
GIHAN PRAMOD <gihanpramod86@gmail.com>
2025-01-15 16:51:14 +0530

    Update SignIndata.php

--- SignIndata.php
+++ SignIndata.php
@@ -1,35 +1,35 @@
 <html>
     <head></head>
     <body>
-        <br>
-        <div style="text-align: center; margin-top: 20px;">
+    <?php
+        session_start();
+        include("connection.php");
+
+        if (isset($_POST['SignIn'])) {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+
+        $sql = "SELECT * FROM account_details WHERE email = '$email' AND password = '$password';";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result && $result->num_rows > 0) {
+             $row = $result->fetch_assoc();
+             $_SESSION['email'] = $row['email'];
+             header("Location: /rentitall-main/index.html"); 
+        exit();
+        } else {
+            echo "Incorrect Email or Password";
+        }
+}
+?>
+        <div style="text-align: left; margin-top: 20px;">
             <button type="button" 
                 onclick="window.location.href='SignUp.html';" 
                 style="padding: 10px 20px; background-color:rgb(5, 81, 16); color: white; border: none; border-radius: 5px; cursor: pointer;">
                 Back to Sign In
     </button>
 </div>
-<?php
-session_start();
-include("connection.php");
 
-if (isset($_POST['SignIn'])) {
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    $sql = "SELECT * FROM account_details WHERE email = '$email' AND password = '$password';";
-    $result = mysqli_query($conn, $sql);
-
-    if ($result && $result->num_rows > 0) {
-        $row = $result->fetch_assoc();
-        $_SESSION['email'] = $row['email'];
-        header("Location: /rentitall-main/index.html"); 
-        exit();
-    } else {
-        echo "Incorrect Email or Password";
-    }
-}
-?>
 
 </body>
 </html>
